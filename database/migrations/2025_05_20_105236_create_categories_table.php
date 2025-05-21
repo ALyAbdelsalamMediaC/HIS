453623e3d1ed987c7b557683b1fd11efe23f25e2
ALyAbdelsalamIcom <155536407+ALyAbdelsalamIcom@users.noreply.github.com>
2025-05-21 13:56:34 +0300

    categories update

--- database/migrations/2025_05_20_105236_create_categories_table.php
+++ database/migrations/2025_05_20_105236_create_categories_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('image_path')->nullable();
-            $table->string('video_path')->nullable();
             $table->text('description')->nullable();
             $table->timestamps();
         });
