4830043dbaaae7cceb46bd53bef2c84c68044f07
MuhammadSamir1212 <mohamedsamirsea@gmail.com>
2025-06-01 10:33:14 +0300

    fix

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,13 +1,8 @@
 <!-- Sidebar Component -->
 <div class="sidebar">
-  <!-- Sidebar Toggle Button -->
-  <div class="sidebar-open-btn">
-    <x-svg-icon name="arrow-right" size="12" color="#b12028" />
-  </div>
   <div class="head">
     <a href="" class="side-logo">
-      <img src="{{ asset('images/logo/logo-full-white.svg') }}" alt="logo" class="logo-large">
-      <img src="{{ asset('images/logo/hansy-small-white.svg') }}" alt="logo-small" class="logo-small">
+      <img src="{{ asset('images/logo/logo-full-white.svg') }}" alt="logo">
     </a>
   </div>
 
@@ -17,70 +12,42 @@
     <nav class="sidebar-menu">
       <ul>
         <li class="sidebar-menu-li">
-          <a href="" class="h3-semibold">
-            <x-svg-icon name="dashboard" size="20" />
+          <a href="" class="h6-semibold">
+            <x-svg-icon name="dashboard" size="18" />
             <span class="sidebar-menu-text">Dashboard</span>
           </a>
         </li>
 
         <li class="sidebar-menu-li">
-          <a href="" class="h3-semibold">
-            <x-svg-icon name="user-check" size="20" />
-            <span class="sidebar-menu-text">Employees</span>
+          <a href="" class="h6-semibold">
+            <x-svg-icon name="user" size="18" />
+            <span class="sidebar-menu-text">User Management</span>
           </a>
         </li>
 
         <li class="sidebar-menu-li">
-          <a href="" class="h3-semibold">
-            <x-svg-icon name="money" size="20" />
-            <span class="sidebar-menu-text">Payroll</span>
+          <a href="" class="h6-semibold">
+            <x-svg-icon name="content" size="18" />
+            <span class="sidebar-menu-text">Content</span>
           </a>
         </li>
         <li class="sidebar-menu-li">
-          <a href="" class="h3-semibold">
-            <x-svg-icon name="user2" size="20" />
-            <span class="sidebar-menu-text">Attendance</span>
+          <a href="" class="h6-semibold">
+            <x-svg-icon name="message" size="18" />
+            <span class="sidebar-menu-text">Comments</span>
           </a>
         </li>
         <li class="sidebar-menu-li">
-          <a href="" class="h3-semibold">
-            <x-svg-icon name="calender" size="20" />
-            <span class="sidebar-menu-text">Meeting</span>
-          </a>
-        </li>
-
-        <li class="sidebar-menu-li">
-          <a href="" class="h3-semibold">
-            <x-svg-icon name="lead" size="20" />
-            <span class="sidebar-menu-text">Leads</span>
+          <a href="" class="h6-semibold">
+            <x-svg-icon name="shield-block" size="18" />
+            <span class="sidebar-menu-text">Blocked Users</span>
           </a>
         </li>
 
         <li class="sidebar-menu-li">
-          <a href="" class="h3-semibold">
-            <x-svg-icon name="history" size="20" />
-            <span class="sidebar-menu-text">Leave Request</span>
-          </a>
-        </li>
-
-        <li class="sidebar-menu-li">
-          <a href="" class="h3-semibold">
-            <x-svg-icon name="analysis-up" size="20" />
-            <span class="sidebar-menu-text">Achievements</span>
-          </a>
-        </li>
-
-        <li class="sidebar-menu-li">
-          <a href="" class="h3-semibold">
-            <x-svg-icon name="map-pin" size="20" />
-            <span class="sidebar-menu-text">Branches</span>
-          </a>
-        </li>
-
-        <li class="sidebar-menu-li">
-          <a href="" class="h3-semibold">
-            <x-svg-icon name="policy" size="20" />
-            <span class="sidebar-menu-text">Private Policy</span>
+          <a href="" class="h6-semibold">
+            <x-svg-icon name="setting" size="18" />
+            <span class="sidebar-menu-text">Settings</span>
           </a>
         </li>
       </ul>
@@ -89,7 +56,10 @@
 
   <!-- Account Section -->
   <div class="sidebar-bottom">
-
+    <a href="" class="h6-semibold">
+      <x-svg-icon name="logout" size="18" />
+      <span class="sidebar-menu-text">Log Out</span>
+    </a>
   </div>
 </div>
 
