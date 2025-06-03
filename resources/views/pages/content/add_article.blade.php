f3668c1209c98d55b666041c46cc816e13e895a6
MuhammadSamir1212 <mohamedsamirsea@gmail.com>
2025-06-03 12:33:32 +0300

    fix

---
+++ resources/views/pages/content/add_article.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('title', 'HIS | Add Article')
+@section('content')
+
+  <section>
+
+  </section>
+
+@endsection
+
+@push('scripts')
+@endpush
---
+++ resources/views/pages/content/articles.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+@section('title', 'HIS | Videos')
+@section('content')
+
+  <section>
+
+    <div class="d-flex justify-content-between align-items-start">
+    <div>
+      <h2 class="h2-semibold" style="color:#35758C;">Content Management</h2>
+      <p class="h5-ragular" style="color:#ADADAD;">Create, update, or save content as draft—all in one place .</p>
+    </div>
+
+    <div class="gap-3 d-flex align-items-center">
+      <x-link_btn href="">
+      <x-svg-icon name="content" size="18" />
+      <span>Add Video</span>
+      </x-link_btn>
+      <x-link_btn href="">
+      <x-svg-icon name="article" size="18" />
+      <span>Add Article</span>
+      </x-link_btn>
+    </div>
+
+    </div>
+
+    <!-- Articles & Videos -->
+
+    <div class="mt-4">
+
+    <!-- Tabs using the component -->
+    <x-tabs_pages :tabs="[
+    ['id' => 'videos', 'label' => 'videos', 'route' => route('content.videos')],
+    ['id' => 'articles', 'label' => 'articles', 'route' => route('content.articles')],
+    ]" activeTab="articles" />
+
+    </div>
+
+  </section>
+
+@endsection
+
+@push('scripts')
+  <script src="{{ asset('js/filters.js') }}"></script>
+@endpush
