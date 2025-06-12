2b52b659ad562c699d5753a948ecbf44340fc6da
MuhammadSamir1212 <mohamedsamirsea@gmail.com>
2025-06-12 15:09:59 +0300

    fix

--- resources/views/pages/settings/index.blade.php
+++ resources/views/pages/settings/index.blade.php
@@ -11,7 +11,7 @@
     <h2 class="mt-4 mb-3 h3-semibold">Account Details:</h2>
 
     <div class="settings-links-content">
-    <a href="{{ route('settings.profile') }}" class="settings-links-container">
+    <a href="" class="settings-links-container">
       <span class="gap-2 d-flex align-items-center">
       <span class="setting-icon">
         <x-svg-icon name="user" size="18" color="#35758C" />
@@ -20,6 +20,51 @@
       <span class="d-flex flex-column">
         <span class="h5-semibold" style="color:#000;">Account Details</span>
         <span class="h6-ragular" style="color:#ADADAD;">Manage your account details</span>
+      </span>
+      </span>
+
+      <x-svg-icon name="arrow-right" size="24" color="#000" />
+    </a>
+
+    <a href="" class="settings-links-container">
+      <span class="gap-2 d-flex align-items-center">
+      <span class="setting-icon">
+        <x-svg-icon name="lock" size="18" color="#35758C" />
+      </span>
+
+      <span class="d-flex flex-column">
+        <span class="h5-semibold" style="color:#000;">Password</span>
+        <span class="h6-ragular" style="color:#ADADAD;">Change your password</span>
+      </span>
+      </span>
+
+      <x-svg-icon name="arrow-right" size="24" color="#000" />
+    </a>
+
+    <a href="" class="settings-links-container">
+      <span class="gap-2 d-flex align-items-center">
+      <span class="setting-icon">
+        <x-svg-icon name="bell" size="18" color="#35758C" />
+      </span>
+
+      <span class="d-flex flex-column">
+        <span class="h5-semibold" style="color:#000;">Notification</span>
+        <span class="h6-ragular" style="color:#ADADAD;">Manage your notification settings</span>
+      </span>
+      </span>
+
+      <x-svg-icon name="arrow-right" size="24" color="#000" />
+    </a>
+
+    <a href="" class="settings-links-container">
+      <span class="gap-2 d-flex align-items-center">
+      <span class="setting-icon">
+        <x-svg-icon name="info" size="18" color="#35758C" />
+      </span>
+
+      <span class="d-flex flex-column">
+        <span class="h5-semibold" style="color:#000;">Help Center</span>
+        <span class="h6-ragular" style="color:#ADADAD;">Get help with the app</span>
       </span>
       </span>
 
