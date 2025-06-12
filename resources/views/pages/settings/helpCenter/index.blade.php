7f40959036469bf70433176f156beb3f20f34248
MuhammadSamir1212 <mohamedsamirsea@gmail.com>
2025-06-12 15:04:20 +0300

    fix

---
+++ resources/views/pages/settings/helpCenter/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title', 'HIS | Profile')
+@section('content')
+
+  <section></section>
+
+@endsection
+
+@push('scripts')
+  <script src="{{ asset('js/validations.js') }}"></script>
+@endpush
