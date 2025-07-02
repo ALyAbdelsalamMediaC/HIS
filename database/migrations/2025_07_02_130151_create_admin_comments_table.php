9191ceda23388c59875acaaf69c99eab586bf11f
ALyAbdelsalamMediaC <aly.abdelsalam@mediacreation>
2025-07-02 16:03:00 +0300

    update admin comment

--- database/migrations/2025_07_02_130151_create_admin_comments_table.php
+++ database/migrations/2025_07_02_130151_create_admin_comments_table.php
@@ -11,16 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-         Schema::create('admin_comments', function (Blueprint $table) {
+        Schema::create('admin_comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('media_id')->constrained()->onDelete('cascade');
-            $table->foreignId('parent_id')->nullable()->constrained('comments')->onDelete('cascade');
+            $table->foreignId('parent_id')->nullable()->constrained('admin_comments')->onDelete('cascade');
             $table->text('content');
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('admin_comments');
