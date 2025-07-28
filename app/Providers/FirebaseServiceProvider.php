da62b94ae676ca79b88909f16cd9c055ac29de5f
ALyAbdelsalamMediaC <aly.abdelsalam@mediacreation>
2025-07-28 13:25:40 +0300

    notification updates

---
+++ app/Providers/FirebaseServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Contract\Messaging;
+class FirebaseServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->singleton(Messaging::class, function ($app) {
+            $factory = (new Factory)
+                ->withServiceAccount(config('firebase.projects.default.credentials'));
+            return $factory->createMessaging();
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
