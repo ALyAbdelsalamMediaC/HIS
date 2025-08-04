4b4a6a8de9b2b54e6474e506bdee94150125f83f
ALyAbdelsalamMediaC <aly.abdelsalam@mediacreation>
2025-08-04 16:08:24 +0300

    update month name in add video in dashboard

--- resources/views/pages/content/add_video.blade.php
+++ resources/views/pages/content/add_video.blade.php
@@ -76,9 +76,9 @@
                 <x-text_label for="month" :required="true">Month</x-text_label>
                 <x-select id="month" name="month"
                     :options="[
-                        1 => 'January', 2 => 'February', 3 => 'March', 4 => 'April',
-                        5 => 'May', 6 => 'June', 7 => 'July', 8 => 'August',
-                        9 => 'September', 10 => 'October', 11 => 'November', 12 => 'December'
+                        'January' => 'January', 'February' => 'February', 'March' => 'March', 'April' => 'April',
+                        'May' => 'May', 'June' => 'June', 'July' => 'July', 'August' => 'August',
+                        'September' => 'September', 'October' => 'October', 'November' => 'November', 'December' => 'December'
                     ]"
                     placeholder="Select Month" data-required="true" data-name="Month" />
                 <div id="month-error-container">
